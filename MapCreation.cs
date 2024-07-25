5325e2e23908ff8a8460bff635aeae1bac215cf9
MaJaiii <mahk.0307@gmail.com>
2024-07-25 22:06:25 +0900

    お疲れ様です。マーです。 マップ生成のスクリプトをアップします。確認してください。 やり方： MapCreation.csをUnityプロジェクトのAssetの中に入れてください。 シーンズ内のMain Cameraのインスペクターの「Add Component」して、MapCreationを入力し選択してください。 Unityにプレイし、Enterキーを押して、Consoleに確認してください。
    以上です。

---
+++ MapCreation.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapCreation : MonoBehaviour
+{
+    // Start is called before the first frame update
+    int[,] map;
+    void Start()
+    {
+        map = new int[6, 6];　//マップの初期化
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return)) //Enterキーを押したら
+        {
+            int count = 0;
+            while (!MapGenerate()) count++;　//マップを生成する
+            Debug.Log("Generation time: "+count++); //マップが生成された回数の表示
+            string msg = string.Empty;
+            for (int i = 0; i < map.GetLength(0); i++)
+            {
+                for (int j = 0; j < map.GetLength(1); j++)
+                {
+                    msg += map[i,j] + " ";
+                }
+                msg += "\n";
+            }
+            Debug.Log(msg);　//最終基準に満たしたマップを表示する
+        }
+    }
+
+    bool MapGenerate()
+    {
+        for (int i = 0; i < map.GetLength(0); i++)
+        {
+            for (int j = 0; j < map.GetLength(1); j++)
+            {
+                map[i, j] = Random.Range(0, 2);　//マスに0か1を代入する
+            }
+        }
+        for (int i = 0; i < map.GetLength(0)-1; i++)
+        {
+            for (int j = 0; j < map.GetLength(1)-1; j++)
+            {
+                if (map[i,j] + map[i,j+1] + map[i+1,j+1] + map[i+1,j] >= 3)　//もし2*2のマスに1が設定されたマス数が3以上なら
+                {
+                    return false;　//ダメを返す
+                }
+            }
+        }
+        return true;　//OKを返す
+    }
+}
