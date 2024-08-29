022af9c2ef7d82d191f775c257229cfb59f12238
yutil66 <nakaza10.yuta@icloud.com>
2024-08-29 21:30:13 +0900

    ﻿時計完成しました。誰でも変えられるように可変式にしてあります。

---
+++ Assets/Clock/ClockHandsManager.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClockMove : MonoBehaviour
+{
+    [SerializeField] private int TimeNum = 10;            //一周にかかる時間を変えるための変数、単位は 分 。
+
+    private float TimeCalculate = 0;    //タイマーの計算に使う値を渡す変数
+    private bool MoningNight = true;    //朝夜のフラグ
+    public GameObject particleObject;   //transform.RotateのZ座標を取得するためのもの
+
+
+
+    void Start()
+    {
+        // VSyncCount を Dont Sync に変更
+        QualitySettings.vSyncCount = 0;
+
+        //60fpsを目標に設定
+        Application.targetFrameRate = 60;
+
+        //現在の時計が何分設定なのかをデバッグログに表示させる
+        Debug.Log(TimeNum + "分で１周する時計");
+
+        //フラグが朝の状態のことを1度だけ表示
+        if (MoningNight = true)
+        {
+            Debug.Log("朝");
+        }
+
+        //TimeNumに入力された値に
+        TimeCalculate = -1.0f / (TimeNum * 10.0f);
+    }
+
+
+    void Update()
+    {
+        
+        //Z座標を移動させ続ける
+        transform.Rotate(new Vector3(0, 0, TimeCalculate));
+
+
+        //Z座標（Rotation）が0～-180の間はtrue(朝)、１～180の間はfalse（夜）
+        if (gameObject.transform.localEulerAngles.z <= 180 && MoningNight == true && gameObject.transform.localEulerAngles.z != 0)
+        {
+            MoningNight = false;
+            Debug.Log("夜");
+        }
+
+        if (gameObject.transform.localEulerAngles.z <= 0)
+        {
+            MoningNight = true;
+            Debug.Log("朝");
+        }
+    }
+}
--- Assets/Script/Contact.cs
+++ Assets/Script/Contact.cs
@@ -18,6 +18,8 @@ public class Contact : MonoBehaviour
     {
         Debug.Log(collision.gameObject.name); // ぶつかった相手の名前を取得
 
+
+
         if (Input.GetKey(KeyCode.Space))
             {
                 Destroy(gameObject);//このゲームオブジェクトを消滅させる
