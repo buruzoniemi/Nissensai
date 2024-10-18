ef15cf7d570335d8fe79074c11602201d8db57d5
nyankomaru <24CU0130@jec.ac.jp>
2024-10-18 20:35:01 +0900

    カメラとプレイヤーの移動用のスクリプトを足しました。

---
+++ Assets/Script/MainCamera.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCamera : MonoBehaviour
+{
+    [SerializeField] private Transform Player;  //注視対象プレイヤー
+    private float Distance = 35.0f;             //注視対象プレイヤーからカメラを離す距離
+    private Quaternion Vrotation;               //カメラの垂直回転(見下ろし回転)
+
+    void Start()
+    {
+        //垂直回転(X軸を軸とする回転)は、90度見下ろす回転
+        Vrotation = Quaternion.Euler(90, 0, 0);
+
+        //player位置から距離distanceだけ手前に引いた位置を設定します
+        transform.position = Player.position - transform.rotation * Vector3.forward * Distance;
+    }
+}
