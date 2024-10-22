38601b133429cf6da5bcdf5f56667832712eceab
yutil66 <nakaza10.yuta@icloud.com>
2024-10-22 23:18:48 +0900

    ﻿引っこ抜くアニメーションを作りました。問題点も確認できていたものはすべて解決しました。

--- Assets/Script/PullUpAnimation.cs
+++ Assets/Script/PullUpAnimation.cs
@@ -1,9 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+<<<<<<< HEAD
 using System.Net;
 using UnityEngine;
 
 
+=======
+using UnityEngine;
+
+>>>>>>> 31613564 (引っこ抜くアニメーションを作りました。問題点も確認できていたものはすべて解決しました。)
 public class PullUpAnimation : MonoBehaviour
 {
     private Animator p_animator;
@@ -13,13 +18,21 @@ public class PullUpAnimation : MonoBehaviour
     }
 
     private float sec = 0;
+<<<<<<< HEAD
     public bool Finish = false; //�A�j���[�V�������s�������ǂ����̃t���O
+=======
+    private bool Fnish = false; //�A�j���[�V�������s�������ǂ����̃t���O
+>>>>>>> 31613564 (引っこ抜くアニメーションを作りました。問題点も確認できていたものはすべて解決しました。)
 
     void Update()
     {
         //Fnish��ture�ɂȂ�����ܕb���false�ɖ߂�����
         sec += Time.deltaTime;
+<<<<<<< HEAD
         if (Finish == true)
+=======
+        if (Fnish = true)
+>>>>>>> 31613564 (引っこ抜くアニメーションを作りました。問題点も確認できていたものはすべて解決しました。)
         {
             if (sec >= 5f)
             {
@@ -27,6 +40,7 @@ public class PullUpAnimation : MonoBehaviour
                 p_animator.SetBool("PullUp", false);
                 p_animator.SetBool("Lift", false);
 
+<<<<<<< HEAD
             
                 Finish = false;
 
@@ -35,6 +49,11 @@ public class PullUpAnimation : MonoBehaviour
             }
         }
          
+=======
+                sec = 0;
+            }
+        }
+>>>>>>> 31613564 (引っこ抜くアニメーションを作りました。問題点も確認できていたものはすべて解決しました。)
     }
 
     //�G�����I�u�W�F�N�g�̃^�O���J�u���������X�y�[�X�������ƁA
@@ -43,13 +62,21 @@ public class PullUpAnimation : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("kabu"))
         {
+<<<<<<< HEAD
             if (Input.GetKey(KeyCode.Space) || (Input.GetKey(KeyCode.Joystick1Button1)))//�L�[�{�[�h�ƃQ�[���p�b�h�ɑΉ�
+=======
+            if (Input.GetKey(KeyCode.Space))
+>>>>>>> 31613564 (引っこ抜くアニメーションを作りました。問題点も確認できていたものはすべて解決しました。)
             {
                 p_animator.SetBool("PullUp", true);
                 p_animator.SetBool("Lift", true);
 
                 //animator��true�ɂȂ�����Finish��true������
+<<<<<<< HEAD
                 Finish = true;
+=======
+                Fnish = true;
+>>>>>>> 31613564 (引っこ抜くアニメーションを作りました。問題点も確認できていたものはすべて解決しました。)
             }
             else if (Input.GetKey(KeyCode.Joystick1Button1))
             {
